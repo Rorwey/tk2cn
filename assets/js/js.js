c1915f199a6d386f2cad96a12b728925e8c13137
LuoV <rorwey@foxmail.com>
2018-10-08 14:03:09 +0800

    尝试添加自动连接跳转

--- assets/js/js.js
+++ assets/js/js.js
@@ -4,7 +4,7 @@ function siteTime() {
 	var minutes = seconds * 60;
 	var hours = minutes * 60;
 	var days = hours * 24;
-	var mouths=days * 30;
+	var mouths = days * 30;
 	var years = days * 365;
 	var today = new Date();
 	var todayYear = today.getFullYear();
@@ -25,26 +25,26 @@ function siteTime() {
 	var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
 	var diff = t2 - t1;
 	var diffYears = Math.floor(diff / years);
-	var diffMouths = Math.floor((diff / mouths)-diffYears*12);//粗略计算月份	
+	var diffMouths = Math.floor((diff / mouths) - diffYears * 12); //粗略计算月份	
 	var diffDays = Math.floor((diff / days) - diffYears * 365 - diffMouths * 30);
-	var diffDay0 = Math.floor((diff / days) - diffYears * 365);	
+	var diffDay0 = Math.floor((diff / days) - diffYears * 365);
 	var diffHours = Math.floor((diff - (diffYears * 365 + diffDay0) * days) / hours);
 	var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDay0) * days - diffHours * hours) / minutes);
 	var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDay0) * days - diffHours * hours - diffMinutes * minutes) / seconds);
 	//	document.getElementById("sitetime").innerHTML = " 已运行" + /*diffYears+" 年 "+*/ diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
 	document.getElementsByClassName("years")[0].innerHTML = diffYears;
-//	document.getElementsByClassName("mouths")[0].innerHTML = diffMouths;
+	//	document.getElementsByClassName("mouths")[0].innerHTML = diffMouths;
 	document.getElementsByClassName("days")[0].innerHTML = diffDays;
 	document.getElementsByClassName("hours")[0].innerHTML = diffHours;
-document.getElementsByClassName("minutes ")[0].innerHTML = diffMinutes;
-document.getElementsByClassName("seconds")[0].innerHTML = diffSeconds;
+	document.getElementsByClassName("minutes ")[0].innerHTML = diffMinutes;
+	document.getElementsByClassName("seconds")[0].innerHTML = diffSeconds;
 } /*因为建站时间还没有一年，就将之注释掉了。需要的可以取消*/
 
-function getMouthDays(year,mouth) {
+function getMouthDays(year, mouth) {
 	//构造当前日期对象
-//	var date = new Date();
-//	var year = date.getFullYear(); //获取年份
-//	var mouth = date.getMonth() + 1; //获取当前月份
+	//	var date = new Date();
+	//	var year = date.getFullYear(); //获取年份
+	//	var mouth = date.getMonth() + 1; //获取当前月份
 	var days; //定义当月的天数；
 	if(mouth == 2) { //当月份为二月时，根据闰年还是非闰年判断天数
 		days = year % 4 == 0 ? 29 : 28;
@@ -56,4 +56,20 @@ function getMouthDays(year,mouth) {
 		days = 30;
 	}
 	return days;
+}
+
+//获取相对路径
+function  GetUrlRelativePath()　　 {　　　　
+	var  url = document.location.toString();　　　　
+	var  arrUrl = url.split("//");
+
+	　　　　
+	var  start = arrUrl[1].indexOf("/");　　　　
+	var  relUrl = arrUrl[1].substring(start); //stop省略，截取从start开始到结尾的所有字符
+
+	　　　　
+	if(relUrl.indexOf("?") != -1) {　　　　　　
+		relUrl = relUrl.split("?")[0];　　　　
+	}　　　　
+	return  relUrl;　　
 }
---
+++ assets/js/jump.js
@@ -0,0 +1,20 @@
+
+var domain=window.location.host;
+
+var host=domain.split(".");
+console.log(host);
+var flag=0;
+if(host(0)=="luov"||host(1)=="luov"){
+	var mubiao="https://luov.top/";
+	flag=1;
+}else if(host(0)=="xian6ge"||host(1)=="xian6ge"){
+	var mubiao="https://xian6ge.cn/";
+	flag=1;
+}
+console.log(mubiao);
+var other=GetUrlRelativePath();
+console.log(other);
+
+if(flag==1){
+	window.location.href = mubiao+other;
+}
