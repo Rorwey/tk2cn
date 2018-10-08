da254a0f71629a79b747a226e5f510f76f0bad8f
LuoV <rorwey@foxmail.com>
2018-10-08 16:43:41 +0800

    修改网名字

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -1,20 +1,22 @@
-
-var domain=window.location.host;
-
-var host=domain.split(".");
+var domain = window.location.host;
+console.log(domain);
+var host = domain.split(".");
 console.log(host);
-var flag=0;
-if(host[0]=="luov"||host[1]=="luov"){
-	var mubiao="https://luov.top";
-	flag=1;
-}else if(host[0]=="xian6ge"||host[1]=="xian6ge"){
-	var mubiao="https://xian6ge.cn";
-	flag=1;
+var flag = 0;
+if(host[0] == "luov" || host[1] == "luov") {
+	var mubiao = "https://luov.top";
+	flag = 1;
+} else if(host[0] == "xian6ge" || host[1] == "xian6ge") {
+	var mubiao = "https://xian6ge.cn";
+	flag = 1;
 }
+if(domain == "time.luov.top")
+	flag = 0;
+end
 console.log(mubiao);
-var other=GetUrlRelativePath();
+var other = GetUrlRelativePath();
 console.log(other);
 
-if(flag==1){
-	window.location.href = mubiao+other;
+if(flag == 1) {
+	window.location.href = mubiao + other;
 }
