13803d745b73603292538aad7d721f1b91c12e8e
LuoV <rorwey@foxmail.com>
2019-02-11 09:17:45 +0800

    测试

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -5,16 +5,16 @@ console.log(host);
 var flag = 0;
 var mubiao = "https://time.luov.top";
 if(host[0]=="time"&&host[1]=="luov"){
-	flag=0;
+	flag=-1;
 }else if (host[0] == "luov") {
 	mubiao = "https://luov.top";
 	flag = 1;
 }else if(host[0] == "xian6ge") {
 	mubiao = "https://xian6ge.cn";
-	flag = 1;
+	flag = 2;
 }else if(host[1]=="luov"){
 	mubiao= "https://"+host[0]+".luov.top";
-	flag=1;
+	flag=3;
 }else if(host[1]=="xian6ge"){
 	mubiao= "https://"+host[0]+".xian6ge.cn"; 
 }
@@ -23,6 +23,6 @@ console.log(mubiao);
 var other = GetUrlRelativePath();
 console.log(other);
 console.log(flag)
-if (flag == 1) {
+if (flag != 0) {
 	window.location.href = mubiao + other;
 }
