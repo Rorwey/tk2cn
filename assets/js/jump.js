8ed16a198f088b2a8875b51f702ee23981bd865b
LuoV <rorwey@foxmail.com>
2019-02-11 08:52:29 +0800

    新修改跳转逻辑

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -3,32 +3,24 @@ console.log(domain);
 var host = domain.split(".");
 console.log(host);
 var flag = 0;
-if(host[0] == "love" || host[1] == "luov") {
-	var mubiao = "https://love.luov.top";
+var mubiao = "https://time.luov.top";
+if (host[0] == "luov") {
+	mubiao = "https://luov.top";
 	flag = 1;
-}else if(host[0] == "time" || host[1] == "luov") {
-	var mubiao = "https://time.luov.top";
+}else if(host[0] == "xian6ge") {
+	mubiao = "https://xian6ge.cn";
 	flag = 1;
-}else if(host[0] == "luov" || host[1] == "luov") {
-	var mubiao = "https://luov.top";
-	flag = 1;
-} else if(host[0] == "en" && host[1] == "xian6ge") {
-	var mubiao = "https://en.xian6ge.cn";
-	flag = 1;
-} else if(host[0] == "xian6ge") {
-	var mubiao = "https://xian6ge.cn";
-	flag = 1;
-}else{
-	var mubiao = "time.luov.top";
-}
-if(domain == "time.luov.top") {
-	flag = 0;
+}else if(host[1]=="luov"){
+	mubiao= "https://"+host[0]+"luov.top";
+	flag=1;
+}else if(host[1]=="xian6ge"){
+	mubiao= "https://"+host[0]+"xian6ge.cn"; 
 }
 
 console.log(mubiao);
 var other = GetUrlRelativePath();
 console.log(other);
 
-if(flag == 1) {
+if (flag == 1) {
 	window.location.href = mubiao + other;
 }
